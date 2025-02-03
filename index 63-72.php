8dbdf0891f019e5c39c0589ba1a0fc92320d7ba9
Mohammed Khalifah <tu4205521@taibahu.edu.sa>
2025-02-03 16:16:58 +0300

    end 63

--- index 63-72.php
+++ index 63-72.php
@@ -1,2 +1,52 @@
 <?php 
 
+// #063 - Array Functions Part 1 - 4 Functions
+echo "#063 - Array Functions Part 1 - 4 Functions<br>";
+
+$friends=["Mohammed","Osama","Omar","Ali","Gamal"];
+
+echo"<pre>";
+print_r($friends);
+echo"</pre>";
+
+echo"<pre>";
+print_r(array_chunk($friends,2));
+echo"</pre>";
+
+$countries = ["EG" => "Egypt", "KSA" => "Saudi Arabia", "Sy" => "Syria", "USA" => "United States"];
+echo"<pre>";
+print_r($countries);
+echo"</pre>";
+
+echo"<pre>";
+print_r(array_chunk($countries,2));
+echo"</pre>";
+
+echo"<pre>";
+print_r(array_chunk($countries,2,true));
+echo"</pre>";
+
+echo"<pre>";
+print_r(array_change_key_case($countries,CASE_LOWER));
+echo"</pre>";
+
+$keys = ["A", "O", "K"];
+$values = ["Ahmed", "Osama", "Kamal"];
+
+echo"<pre>";
+print_r($keys);
+echo"</pre>";
+echo"<pre>";
+print_r($values);
+echo"</pre>";
+
+echo"<pre>";
+print_r(array_combine($keys,$values));
+echo"</pre>";
+
+$counting = ["B", "A", "A", "B", "B", "B", "C"];
+
+echo"<pre>";
+print_r(array_count_values($counting));
+echo"</pre>";
+
