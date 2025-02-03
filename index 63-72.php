590cab1aeb4d202f4e5f201c0c8fc159d8f78339
Mohammed Khalifah <tu4205521@taibahu.edu.sa>
2025-02-03 16:34:42 +0300

    end 64

--- index 63-72.php
+++ index 63-72.php
@@ -50,3 +50,62 @@ echo"<pre>";
 print_r(array_count_values($counting));
 echo"</pre>";
 
+// #064 - Array Functions Part 2 - 5 Functions
+echo "#064 - Array Functions Part 2 - 5 Functions<br>";
+$family = ["Osama", "Ahmed", "Sameh", "Mahmoud", "Gamal", ["Eman", "Noha"]];
+
+echo"<pre>";
+print_r($family);
+echo"</pre>";
+
+echo"<pre>";
+print_r(array_reverse($family));
+echo"</pre>";
+
+
+echo"<pre>";
+print_r(array_reverse($family,true));
+echo"</pre>";
+
+echo"<pre>";
+print_r(array_flip($family)); //عكس المفتاح والقيمة
+echo"</pre>";
+
+$counting = [
+    "A",
+    "B",
+    "C",
+    [1, 2, 3]
+];
+
+  echo"<pre>";
+  print_r($counting);
+  echo"</pre>";
+  echo "count(counting) ".count($counting)."<br>";
+  echo "count(counting,1) ".count($counting,1)."<br>";
+
+  $search = ["1", 2, 3, 4];
+  echo"<pre>";
+  print_r($search);
+  echo"</pre>";
+
+  if(in_array(1,$search)){
+    echo "The Element Is Found<br>";
+  }
+  if(!in_array(1,$search,true)){
+    echo "The Element Is NOT Found<br>";
+  }
+
+  $courses = [
+    "Javascript" => 95,
+    "PHP" => 100,
+    "HTML" => 60,
+    "CSS" => 65
+  ];
+  echo"<pre>";
+  print_r($courses);
+  echo"</pre>";
+
+  if (array_key_exists("PHP", $courses)) {
+    echo "The Course Is Found And The Price Is: " . $courses["PHP"]."<br>";
+  }
