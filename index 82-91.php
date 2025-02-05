0890e148f776dafeed0411a9fccf4e35a7316636
Mohammed Khalifah <tu4205521@taibahu.edu.sa>
2025-02-05 19:07:12 +0300

    83

--- index 82-91.php
+++ index 82-91.php
@@ -15,3 +15,23 @@ var_dump(filesize("index 73-81.php"));
 
 // #083 - Is_Dir, MkDir, RmDir
 echo '<br>#083 - Is_Dir, MkDir, RmDir<br>';
+
+/*
+    File System Functions
+    - is_dir(Name)
+    --- Tells If The Filename Is A Directory
+    - mkdir(Path[Required], Mode[Optional] = 0777, Recursive[Optional], Context[Optional])
+    --- Permissions Is Ignored On Windows
+    --- Permissions Is Octal Number Start With 0
+    ------ Second Number Is => Owner Permission
+    ------ Third Number Is => User Group Permission
+    ------ Fourth Number Is => Others Permissions
+    - rmdir(Name[Required], Context[Optional])
+  */
+echo'<br>var_dump(is_dir("lesson")) => ';
+var_dump(is_dir("lesson"));
+
+echo'<br>var_dump(is_dir("fd")) => ';
+var_dump(is_dir("fd"));
+
+mkdir('/Applications/XAMPP/xamppfiles/htdocs/learning/ass');
