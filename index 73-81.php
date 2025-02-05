0730416e82789962c17bf56a1fb7df4c9a2fcfab
Mohammed Khalifah <tu4205521@taibahu.edu.sa>
2025-02-05 17:37:27 +0300

    end 81

--- index 73-81.php
+++ index 73-81.php
@@ -68,7 +68,7 @@ echo "<pre>filter_list()";
 print_r(filter_list());
 echo "</pre>";
 
-echo 'filter_id("boolean") => ' . filter_id("boolean"). "<br>";
+echo 'filter_id("boolean") => ' . filter_id( "boolean"). "<br>";
 
 $value=true;
 if(filter_var($value,FILTER_VALIDATE_BOOL)){
@@ -97,4 +97,132 @@ $ip = "192.168.2.1";
 echo '<br>$ip = "192.168.2.1" => ';
 
 var_dump(filter_var($ip, FILTER_VALIDATE_IP, ["flags" => FILTER_NULL_ON_FAILURE | FILTER_FLAG_IPV4]));
+
+// #079 - filter_var Validate Filters Part 2
+echo "<br>#079 - filter_var Validate Filters Part 2<br>";
+/*
+    Filter Functions
+
+    - filter_var(Value[Required], Filter[Optional], Options[Optional])
+    --- Validate Filters
+
+    ------ FILTER_VALIDATE_EMAIL
+    ---------- Flag => FILTER_NULL_ON_FAILURE
+
+    ------ FILTER_VALIDATE_INT
+    ---------- Flag => FILTER_NULL_ON_FAILURE
+    ---------- Options => min_range
+    ---------- Options => max_range
+    ---------- Convert To Int On Success
+
+    ------ FILTER_VALIDATE_FLOAT
+    ---------- Flag => FILTER_NULL_ON_FAILURE
+    ---------- Options => min_range
+    ---------- Options => max_range
+    ---------- Convert To Float On Success
+  */
+  $email = "mmmak2017@gmail.com";
+  echo '<br>$email = "mmmak2017@gmail.com" => ';
+  var_dump(filter_var($email,FILTER_VALIDATE_EMAIL,FILTER_NULL_ON_FAILURE));
+
+  $int = "100";
+  echo '<br>$int = "100" => ';
+  var_dump(filter_var($int,FILTER_VALIDATE_INT,FILTER_NULL_ON_FAILURE));
+
+  $int = "1020";
+  echo '<br>$int = "1020" => ';
+  var_dump(filter_var(
+    $int,
+    FILTER_VALIDATE_INT,
+    ["flags" => FILTER_NULL_ON_FAILURE | FILTER_NULL_ON_FAILURE,
+    "options"=> ["min_range"=>50, "max_range"=>100]]
+  ));
+
+  $int = "100";
+  echo '<br>$int = "100" => ';
+  var_dump(filter_var(
+    $int,
+    FILTER_VALIDATE_INT,
+    ["flags" => FILTER_NULL_ON_FAILURE | FILTER_NULL_ON_FAILURE,
+    "options"=> ["min_range"=>50, "max_range"=>100]]
+  ));
+  $float = "100.0";
+  echo '<br>$int = "100.0" => ';
+  var_dump(filter_var(
+    $float,
+    FILTER_VALIDATE_FLOAT,
+    ["flags" => FILTER_NULL_ON_FAILURE | FILTER_NULL_ON_FAILURE,
+    "options"=> ["min_range"=>50, "max_range"=>100]]
+  ));
+
+  // #080 - filter_var Sanitize Filters
+  echo "<br>#080 - filter_var Sanitize Filters<br>";
+
+  /*
+    Filter Functions
+
+    - filter_var(Value[Required], Filter[Optional], Options[Optional])
+    --- Sanitize Filters
+
+    ------ FILTER_SANITIZE_EMAIL
+    ------ Remove All But Letters + Digits + !#$%&#038;'*+-=?^_`{|}~@.[]
+    ------ Test ¥
+
+    ------ FILTER_SANITIZE_NUMBER_INT
+    --------- Remove All But Digits, +, -
+
+    ------ FILTER_SANITIZE_NUMBER_FLOAT
+    --------- Remove ALL But Digits, +, - And Optionally [.,eE]
+    --------- Flags => FILTER_FLAG_ALLOW_THOUSAND
+    --------- Flags => FILTER_FLAG_ALLOW_FRACTION
+
+    ------ FILTER_SANITIZE_URL
+    ------ Remove All But Letters + Digits + $-_.+!*'(),{}|\\^~[]`<>#%";/?:@&=.
+    ------ Test ¥ + Space
+  */
+  $email="o@ . sa";
+  echo '<br>$email="o@ . sa" => ';
+  var_dump(filter_var($email,FILTER_SANITIZE_EMAIL));
+
+  $int=100;
+  echo '<br>$int=100 => ';
+  var_dump(filter_var($int,FILTER_SANITIZE_NUMBER_INT));
+
+  $int="dskmdskm 10+ djfnjdnfjd 20";
+  echo '<br>$int="dskmdskm 10+ djfnjdnfjd 20" => ';
+  var_dump(filter_var($int,FILTER_SANITIZE_NUMBER_INT));
+
+  $float = "1,950,150.6541";
+  echo '<br>$float = "1,950,150.6541" => ';
+ var_dump(filter_var(
+  $float,
+  FILTER_SANITIZE_NUMBER_FLOAT,
+  ["flags"=> FILTER_FLAG_ALLOW_THOUSAND|FILTER_FLAG_ALLOW_FRACTION]
+ )) ;
+
+
+
+//  4
+$array= filter_list();
+$numbers=[];
+foreach($array as $key => $value){
+  $numbers[$key]=(int)filter_id($value);
+}
+echo "<pre>";
+print_r($numbers);
+echo "</pre>";
+echo '<pre>array_combine($numbers,$array)';
+print_r(array_combine($numbers,$array));
+echo "</pre>";
+
+
+//  #081 - filter_input
+echo "<br>#081 - filter_input<br>";
+echo filter_input(INPUT_GET,"num",FILTER_VALIDATE_INT)
+?>
+<form action="" method="GET">
+  <input type="text" name="num">
+  <input type="submit" name="Send">
+</form>
+
 
